be3d7647fd0b6f21acf74de1c4fbc2c499010f83
Leangheng-huge <heng525555@gmail.com>
2025-11-23 10:42:27 +0700

    Fixing an error

--- src/main/java/com/romantic/proposal/ProposalApplication.java
+++ src/main/java/com/romantic/proposal/ProposalApplication.java
@@ -2,13 +2,27 @@ package com.romantic.proposal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProposalApplication {
 
-	public static void main(String[] args) {
-
+    public static void main(String[] args) {
         SpringApplication.run(ProposalApplication.class, args);
-	}
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:5500") // your frontend
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
